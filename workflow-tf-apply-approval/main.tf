6ea66b2e622c7ab6cbbf66344594d8f907599134
Jun Ye <jye@aviatrix.com>
2026-07-08 14:38:02 -0400

    add transit and attach spoke to it

--- workflow-tf-apply-approval/main.tf
+++ workflow-tf-apply-approval/main.tf
@@ -2,10 +2,24 @@ module "mc-spoke" {
   source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
   version = "1.7.1"
 
-  account  = var.aviatrix_account
-  region   = "us-east-1"
-  name     = "ue1spoke1"
-  cloud    = "AWS"
-  cidr     = "10.1.1.0/24"
-  attached = false
+  account    = var.aviatrix_account
+  region     = "us-east-1"
+  name       = "ue1spoke1"
+  cloud      = "AWS"
+  cidr       = "10.1.1.0/24"
+  attached   = false
+  ha_gw      = false
+  transit_gw = module.mc-transit.transit_gateway.gw_name
+}
+
+module "mc-transit" {
+  source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
+  version = "2.6.0"
+
+  account = var.aviatrix_account
+  region  = "us-east-1"
+  name    = "ue1transit1"
+  cloud   = "AWS"
+  cidr    = "10.1.2.0/24"
+  ha_gw   = false
 }
