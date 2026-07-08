5feb6bda48fed77150b084ec15dded6e9b18774d
Jun Ye <jye@aviatrix.com>
2026-07-08 14:57:44 -0400

    downsize gateways to t3.small

--- workflow-tf-apply-approval/main.tf
+++ workflow-tf-apply-approval/main.tf
@@ -2,24 +2,26 @@ module "mc-spoke" {
   source  = "terraform-aviatrix-modules/mc-spoke/aviatrix"
   version = "1.7.1"
 
-  account    = var.aviatrix_account
-  region     = "us-east-1"
-  name       = "ue1spoke1"
-  cloud      = "AWS"
-  cidr       = "10.1.1.0/24"
-  attached   = true
-  ha_gw      = false
-  transit_gw = module.mc-transit.transit_gateway.gw_name
+  account       = var.aviatrix_account
+  region        = "us-east-1"
+  name          = "ue1spoke1"
+  cloud         = "AWS"
+  cidr          = "10.1.1.0/24"
+  attached      = true
+  ha_gw         = false
+  transit_gw    = module.mc-transit.transit_gateway.gw_name
+  instance_size = "t3.small"
 }
 
 module "mc-transit" {
   source  = "terraform-aviatrix-modules/mc-transit/aviatrix"
   version = "2.6.0"
 
-  account = var.aviatrix_account
-  region  = "us-east-1"
-  name    = "ue1transit1"
-  cloud   = "AWS"
-  cidr    = "10.1.2.0/24"
-  ha_gw   = false
+  account       = var.aviatrix_account
+  region        = "us-east-1"
+  name          = "ue1transit1"
+  cloud         = "AWS"
+  cidr          = "10.1.2.0/24"
+  ha_gw         = false
+  instance_size = "t3.small"
 }
